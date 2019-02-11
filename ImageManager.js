09028b1bd3187c48dbf8aa23b1d420eaa39a0fc6
Krzysztof Nowicki <krzysztof11nowicki@gmail.com>
2019-02-11 23:49:02 +0100

    part1

--- ImageManager.js
+++ ImageManager.js
@@ -1,40 +1,12 @@
-function init(){
-    const canvas = initFullScreenCanvas("mainCanvas");
-
-    const imageManager = new imageManager();
-
-    imageManager.load({
-        "arch-left" : "img/arch-left.png",
-        "arch-right" : "img/arch-right.png",
-        "knight": " img/knight.png"
-    }, onDone, onProgress);
-}
-
-function onProgress(loaded, total, key, path, success) {
-    if (success) {
-        console.log("loaded " + loaded + " from " + total + " pictures");
-    } else {
-        console.log("Error: Not loaded picture " + path);
-    }
-}
-
-function onDone() {
-    console.log("All pictures loaded");
-}
-
 function ImageManager(placeholderDataUri) {
-    this._images = {
-        "arch-left" : [],
-        "arch-right" : [],
-        "knight": []
-    }
+    this._images = {};
+	if (placeholderDataUri) {
+		this._placeholder = new Image();
+		this._placeholder.src = placeholderDataUri;
+	}
 }
 
 _p = ImageManager.prototype;
-
-_p.get = function(key){
-    return this._images[key];
-}
 
 _p.load = function(images, onDone, onProgress){
     const queue = [];
@@ -58,7 +30,7 @@ _p.load = function(images, onDone, onProgress){
     for (let i = 0; i < queue.length; i++) {
         this._loadItem(queue[i], itemCounter, onDone, onProgress);        
     }
-}
+};
 
 _p._onLoadItem = function(queueItem, itemCounter, onDone, onProgress){
     let self = this;
@@ -70,7 +42,7 @@ _p._onLoadItem = function(queueItem, itemCounter, onDone, onProgress){
 
     img.onerror = function(){
         self._onItemLoaded(queueItem, itemCounter, onDone, onProgress, false);
-    }
+    };
     img.src = queueItem.path;
 };
 
@@ -81,3 +53,7 @@ _p.onItemLoaded = function(queueItem, itemCounter, onDone, onProgress, success){
         onDone && onDone();
     }
 };
+
+_p.get = function(key){
+    return this._images[key];
+}
