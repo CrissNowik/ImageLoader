b2ce7e42d33b080b189ffc64d15493758f869a5f
Krzysztof Nowicki <krzysztof11nowicki@gmail.com>
2019-02-12 07:33:55 +0100

    debug + fix + doc p1

--- ImageManager.js
+++ ImageManager.js
@@ -1,3 +1,7 @@
+/*****
+ * Image Manager obcjet is responsible for loading images, informing about progress and ending the process
+ * 
+ */
 function ImageManager(placeholderDataUri) {
     this._images = {};
 	if (placeholderDataUri) {
@@ -7,6 +11,13 @@ function ImageManager(placeholderDataUri) {
 }
 
 _p = ImageManager.prototype;
+
+/****
+ * load - function - public method used for preparing image queue and firing _loadItem method on each of images
+ * @param - images - object with key-value pairs 
+ * @param - onDone - function informing about load success,
+ * @param - onProgress -function informin about load progress or fail
+*/
 
 _p.load = function(images, onDone, onProgress){
     const queue = [];
@@ -32,6 +43,31 @@ _p.load = function(images, onDone, onProgress){
     }
 };
 
+/****
+ * 
+ * 
+*/
+
+_p._loadItem = function(queueItem, itemCounter, onDone, onProgress) {
+	var self = this;
+	var img = new Image();
+	img.onload = function() {
+		self._images[queueItem.key] = img;
+		self._onItemLoaded(queueItem, itemCounter, onDone, onProgress, true);
+	};
+
+	img.onerror = function() {
+		self._images[queueItem.key] = self._placeholder ? self._placeholder : null;
+		self._onItemLoaded(queueItem, itemCounter, onDone, onProgress, false);
+	};
+	img.src = queueItem.path;
+};
+
+/****
+ * 
+ * 
+*/
+
 _p._onLoadItem = function(queueItem, itemCounter, onDone, onProgress){
     let self = this;
     let img = new Image();
@@ -46,13 +82,23 @@ _p._onLoadItem = function(queueItem, itemCounter, onDone, onProgress){
     img.src = queueItem.path;
 };
 
-_p.onItemLoaded = function(queueItem, itemCounter, onDone, onProgress, success){
+/****
+ * 
+ * 
+*/
+
+_p._onItemLoaded = function(queueItem, itemCounter, onDone, onProgress, success){
     itemCounter.loaded++;
     onProgress && onProgress(itemCounter.loaded, itemCounter.total, queueItem.key, queueItem.path, success);
     if (itemCounter.loaded === itemCounter.total) {
         onDone && onDone();
     }
 };
+
+/****
+ * simple function returning loaded image suitable to key
+ * @param picture alias 
+ */
 
 _p.get = function(key){
     return this._images[key];
--- init.js
+++ init.js
@@ -1,2 +1,39 @@
+window.addEventListener('load', init);
 
+function init() {
+    const canvas = initFullScreenCanvas("mainCanvas");
 
+    const imageManager = new ImageManager();
+
+    imageManager.load({
+        "arch-left": "img/arch-left.png",
+        "arch-right": "img/arch-right.png",
+        "knight": " img/knight.png"
+    }, onDone, onProgress);
+};
+
+function onProgress(loaded, total, key, path, success) {
+    if (success) {
+        console.log("loaded " + loaded + " from " + total + " pictures");
+    } else {
+        console.log("Error: Not loaded picture " + path);
+    }
+}
+
+function onDone() {
+    console.log("All pictures loaded");
+};
+
+function initFullScreenCanvas(canvasId) {
+    var canvas = document.getElementById(canvasId);
+    resizeCanvas(canvas);
+    window.addEventListener("resize", function () {
+        resizeCanvas(canvas);
+    });
+    return canvas;
+}
+
+function resizeCanvas(canvas) {
+    canvas.width = document.width || document.body.clientWidth;
+    canvas.height = document.height || document.body.clientHeight;
+}
